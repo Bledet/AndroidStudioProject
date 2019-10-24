b4c34d78ffd2a9a9f36f0e5361569c112aed28bf
Benoît Ledet <50702605+Bledet@users.noreply.github.com>
2019-10-24 18:48:57 +0200

    Avancement majeur
    Connection des boutons et gestions des TextView pour la perte de vie, gain de puissance et piece

--- ProjetLedet/app/src/main/java/com/example/projetledet/FightActivity.java
+++ ProjetLedet/app/src/main/java/com/example/projetledet/FightActivity.java
@@ -2,13 +2,100 @@ package com.example.projetledet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class FightActivity extends AppCompatActivity {
+
+    private TextView vPuissance;
+    private TextView vVie;
+    private String result;
+    private String nbPiece;
+    private TextView vPowerEnnemis;
+
+    private Button attaqueButton;
+    private Button fuiteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fight);
+
+        vPuissance    = (TextView) findViewById(R.id.fpuissance);
+        vVie          = (TextView) findViewById(R.id.fvie);
+        attaqueButton = (Button) findViewById(R.id.attaqueButton);
+        fuiteButton   = (Button) findViewById(R.id.fuiteButton);
+        vPowerEnnemis = (TextView) findViewById(R.id.puissance_adverse);
+
+        Intent data = getIntent();
+        vPuissance.setText(data.getStringExtra("power"));
+        vVie.setText(data.getStringExtra("life"));
+        result = data.getStringExtra("res");
+        nbPiece = data.getStringExtra("nbPiece");
+        vPowerEnnemis.setText(data.getStringExtra("powerEnnemis"));
+
+        attaqueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rand = new Random();
+                int randj = rand.nextInt(1 - 0 + 1) + 0;
+                rand = new Random();
+                int randa = rand.nextInt(1 - 0 + 1) + 0;
+                int res = Integer.parseInt(vPuissance.getText().toString())*randj - Integer.parseInt(vPowerEnnemis.getText().toString())*randa;
+                if(res>=0){
+                    result = "VICTOIRE !!!";
+                    int puissance = Integer.parseInt(vPuissance.getText().toString());
+                    puissance += 10;
+                    vPuissance.setText(Integer.toString(puissance));
+
+                    int nb = Integer.parseInt(nbPiece);
+                    nb -= 1;
+                    nbPiece = Integer.toString(nb);
+
+                }else{
+
+                    int vie = Integer.parseInt(vVie.getText().toString());
+                    vie -= 3;
+                    vVie.setText(Integer.toString(vie));
+                    result = "DEFAITE...";
+
+                }
+
+                Intent intent = new Intent(FightActivity.this, MainActivity.class);
+                intent.putExtra("rPower", vPuissance.getText().toString());
+                intent.putExtra("rLife", vVie.getText().toString());
+                intent.putExtra("rRes", result);
+                intent.putExtra("rNbPiece", nbPiece);
+                setResult(Activity.RESULT_OK,intent);
+                finish();
+
+            }
+        });
+
+        fuiteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int vie = Integer.parseInt(vVie.getText().toString());
+                vie -= 1;
+                vVie.setText(Integer.toString(vie));
+                result = "Vous avez pris la fuite...";
+
+                Intent intent = new Intent(FightActivity.this, MainActivity.class);
+                intent.putExtra("rPower", vPuissance.getText().toString());
+                intent.putExtra("rLife", vVie.getText().toString());
+                intent.putExtra("rRes", result);
+                intent.putExtra("rNbPiece", nbPiece);
+                setResult(Activity.RESULT_OK,intent);
+                finish();
+            }
+        });
+
+
     }
 }
--- ProjetLedet/app/src/main/java/com/example/projetledet/MainActivity.java
+++ ProjetLedet/app/src/main/java/com/example/projetledet/MainActivity.java
@@ -1,21 +1,121 @@
 package com.example.projetledet;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import android.os.Bundle;
 import android.view.MenuInflater;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.Random;
+import java.util.Vector;
+
+import static java.lang.Math.random;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView vPuissance;
+    private TextView vVie;
+    private TextView result;
+    private TextView nbPiece;
+
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+    private Button button10;
+    private Button button11;
+    private Button button12;
+    private Button button13;
+    private Button button14;
+    private Button button15;
+    private Button button16;
+
+    public static final int FIGHT = 1;
+
+    // Référencement des puissance aléatoire
+    Vector powerList = new Vector();
+
+    private void initList(){
+
+        for(int i=0; i<16; i++){
+
+            Random rand = new Random();
+            int puissance_ennemis = rand.nextInt(150 - 1 + 1) + 1;
+            String power = Integer.toString(puissance_ennemis);
+
+            powerList.add(power);
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initList();
+
+        for(int i=0; i<powerList.size(); i++){
+            System.out.println("Case"+i+" : " + powerList.get(i));
+        }
+
+        vPuissance = (TextView) findViewById(R.id.puissance);
+        vVie = (TextView) findViewById(R.id.vie);
+        result = (TextView) findViewById(R.id.resultat_combat);
+        nbPiece = (TextView) findViewById(R.id.piece_non_explorer);
+
+        button1 = (Button) findViewById(R.id.room1);
+        button2 = (Button) findViewById(R.id.room2);
+        button3 = (Button) findViewById(R.id.room3);
+        button4 = (Button) findViewById(R.id.room4);
+        button5 = (Button) findViewById(R.id.room5);
+        button6 = (Button) findViewById(R.id.room6);
+        button7 = (Button) findViewById(R.id.room7);
+        button8 = (Button) findViewById(R.id.room8);
+        button9 = (Button) findViewById(R.id.room9);
+        button10 = (Button) findViewById(R.id.room10);
+        button11 = (Button) findViewById(R.id.room11);
+        button12 = (Button) findViewById(R.id.room12);
+        button13 = (Button) findViewById(R.id.room13);
+        button14 = (Button) findViewById(R.id.room14);
+        button15 = (Button) findViewById(R.id.room15);
+        button16 = (Button) findViewById(R.id.room16);
+
+    }
+
+    // Fonction de gestion d'entrée dans une salle
+    public void onClick(View v){
+
+            String getRoom = getResources().getResourceEntryName(v.getId()); //On récupère le nom de l'ID : room+"i"
+            char last = getRoom.charAt(getRoom.length()-1);                  //On récupère le numéro de la pièce: "i"
+            int index = Character.getNumericValue(last)-1;                   //On convertit en int et on enlève 1 pour l'adapté au tableau
+
+            System.out.println("IDENTITEEEEEEEEEE :" + last);
+
+            Intent intent = new Intent(MainActivity.this, FightActivity.class);
+            intent.putExtra("power", vPuissance.getText().toString());
+            intent.putExtra("life", vVie.getText().toString());
+            intent.putExtra("res", result.getText().toString());
+            intent.putExtra("nbPiece", nbPiece.getText().toString());
+            intent.putExtra("powerEnnemis", powerList.get(index).toString()); //On récupère la puissance de l'adversaire dans la powerList
+            startActivityForResult(intent, FIGHT);
     }
 
     @Override
@@ -37,6 +137,23 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onContextItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_CANCELED) {
+            Toast.makeText(MainActivity.this, "Cancelled.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (requestCode == FIGHT) {
+            vPuissance.setText(data.getStringExtra("rPower"));
+            vVie.setText(data.getStringExtra("rLife"));
+            result.setText(data.getStringExtra("rRes"));
+            nbPiece.setText(data.getStringExtra("rNbPiece"));
         }
     }
 }
